8acaae93f155aa31560fb8dd0f554e38f23a6a58
Chris Or <chris.o.riordan11@gmail.com>
2024-11-28 20:00:03 +0100

    update to db creation scripts

--- db/create-snap-shot-table.sql
+++ db/create-snap-shot-table.sql
@@ -3,9 +3,9 @@ DROP TABLE IF EXISTS snap_shots;
 CREATE TABLE snap_shots (
   id UUID NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
   batch_id UUID NOT NULL,
-  name VARCHAR(100) NOT NULL,
-  path VARCHAR(200) NOT NULL,
-  snap_shot_type VARCHAR(100) NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  path VARCHAR(255) NOT NULL,
+  snap_shot_type VARCHAR(255) NOT NULL,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
