3304edb6da639d8a5a12e569b25f1f368aa0ee71
Chris Or <chris.o.riordan11@gmail.com>
2024-12-01 16:35:55 +0100

    Tweaks to historical page

--- fe/src/features/CompareImagesHistoricalList/pages/CompareImagesHistoricalList.tsx
+++ fe/src/features/CompareImagesHistoricalList/pages/CompareImagesHistoricalList.tsx
@@ -30,7 +30,7 @@ export default function CompareImagesHistoricalList() {
         <Sider>
           <Navigation />
         </Sider>
-        <Content className="p-4">
+        <Content className="p-4 space-y-4">
         <h2 className='text-2xl font-semibold'>Historical Jobs</h2>
 
           <Loadable isLoading={isLoading}>
